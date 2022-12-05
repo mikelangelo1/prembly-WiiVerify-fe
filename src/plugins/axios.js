9cfc49be6583e6973d94d7504d6cf579b0374559
mikelangelo1 <michael.oluwasola@stu.cu.edu.ng>
2022-12-05 18:11:05 +0100

    fix bug

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-let baseURL = "https://swift-pay.onrender.com/";
+let baseURL = "https://swift-pay.onrender.com/api/";
 
 if (window.location.origin?.includes("localhost")) {
-  baseURL = "https://swift-pay.onrender.com/";
+  baseURL = "https://swift-pay.onrender.com/api/";
 }
 
 const instance = axios.create({
